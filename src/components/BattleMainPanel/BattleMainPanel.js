78bc32b3b85e424ff4eac3dfb7a072000d0e7794
QQ <cchangwo@yahoo.com.tw>
2021-02-16 15:12:13 +0800

    Use GIF and test animation

--- src/components/BattleMainPanel/BattleMainPanel.js
+++ src/components/BattleMainPanel/BattleMainPanel.js
@@ -1,9 +1,96 @@
 import React from "react";
-import { Stage, Layer } from "react-konva";
+import { Stage, Layer, Rect,Image } from "react-konva";
 import MosterInformation from "./MosterInformation.js"
 // gifler will be imported into global window object
+// gifler will be imported into global window object
+import "gifler";
+
 
 import ReactDOM from 'react-dom';
+
+
+class MyRect extends React.Component {
+
+  startX = this.props.startX;
+  startY = this.props.startY;
+
+  changeSize = () => {
+    // to() is a method of `Konva.Node` instances
+    this.rect.to({
+      scaleX: Math.random() + 0.8,
+      scaleY: Math.random() + 0.8,
+      duration: 0.2
+    });
+  };
+
+  componentDidMount() {
+    this.rect.to({
+      scaleX: 1,
+      scaleY: 1,
+      x:400,
+      y:10,
+      // visible: false,
+      duration: 2,
+      onFinish: () => {
+        this.rect.to({
+          // scaleX: 1,
+          // scaleY: 1,
+          // visible: false,
+          opacity: 0,
+        });
+        console.log("Finished");
+      },
+    });
+
+  }
+
+  render() {
+    return (
+      <Rect
+        ref={node => {
+          this.rect = node;
+        }}
+        width={100}
+        height={100}
+        fill="green"
+        draggable
+        onDragEnd={this.changeSize}
+        onDragStart={this.changeSize}
+        x={this.startX+50}
+        y={this.startY+50}
+      />
+    );
+  }
+}
+
+
+// the first very simple and recommended way:
+const GIF = ({ src,x,y }) => {
+  const imageRef = React.useRef(null);
+  const canvas = React.useMemo(() => {
+    const node = document.createElement("canvas");
+    return node;
+  }, []);
+
+  React.useEffect(() => {
+    // save animation instance to stop it on unmount
+    let anim;
+    window.gifler(src).get(a => {
+      anim = a;
+      anim.animateInCanvas(canvas);
+      anim.onDrawFrame = (ctx, frame) => {
+        ctx.drawImage(frame.buffer, frame.x, frame.y);
+        imageRef.current.getLayer().draw();
+      };
+    });
+    return () => anim.stop();
+  }, [src, canvas]);
+
+  
+  return <Image x={x} y={y}             scaleX={1.8}
+  scaleY={1.8} image={canvas}  ref={imageRef} />;
+};
+
 
 
 class BattleMainPanel extends React.Component {
@@ -42,12 +129,16 @@ class BattleMainPanel extends React.Component {
     const panelHeight = this.state.stageHeight;
 
     const monsterWidth = panelWidth/2;
-    const enemyPokePosition = {"x":(panelWidth-monsterWidth-20),"y":(0+20)};
+    // const enemyPokePosition = {"x":(panelWidth-monsterWidth-20),"y":(0+20)};
     
-    const ourPokePosition   = {"x":(0+20)         ,"y":(panelHeight-monsterWidth+monsterWidth*2/8)};
+    // const ourPokePosition   = {"x":(0+20)         ,"y":(panelHeight-monsterWidth+monsterWidth*2/8)};
+
+    const enemyPokePosition = {"x":(350),"y":(0+20)};
+    const ourPokePosition   = {"x":(20)         ,"y":(400)};
 
     const enemyInformationPosition  = {"x":30,"y":(0+20)};
     const ourInformationPosition    = {"x":(350),"y":(450)};
+    
 
     const {enemyImg,enemyName,enemyCurrentHP,enemyTotalHP} = this.props
     const {ourImg,ourName,ourCurrentHP,ourTotalHP} = this.props
@@ -74,11 +165,12 @@ class BattleMainPanel extends React.Component {
           x={ourInformationPosition.x} 
           y={ourInformationPosition.y} 
           monsterName={ourName} currentHP={ourCurrentHP} totalHP={ourTotalHP}/>
+       
         <Layer>
          
-
+       
          
-          <Portal>
+          {/* <Portal>
             
             <img alt="enemy"
               style={{
@@ -98,9 +190,22 @@ class BattleMainPanel extends React.Component {
               }}
               src={ourImg}
             />
-          </Portal>
-          
+          </Portal> */}
+
+        <GIF src={ourImg} 
+          x={enemyPokePosition.x}
+          y={enemyPokePosition.y}>
+        </GIF>        
+        <GIF src={ourImg} 
+          x={ourPokePosition.x}
+          y={ourPokePosition.y}>
+        </GIF>
+        
         </Layer>
+
+        <Layer >
+         <MyRect startX={ ourPokePosition.x } startY={ourPokePosition.y}/>
+         </Layer>
       </Stage>
       </div>
     );
